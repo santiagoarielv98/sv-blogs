4c7afc26495641db9b39066e99e09b59f15b201f
Santiago Ariel <santiagoarielv98@gmail.com>
2024-12-27 12:13:15 -0300

    Implement NextAuth integration; create authentication provider and update layout for session management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Outfit } from "next/font/google";
 
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
+import NextAuthProvider from "./provider";
+import { auth } from "@/lib/auth";
 
 export const metadata: Metadata = {
   title: "Sv - Blogs",
@@ -11,16 +13,17 @@ export const metadata: Metadata = {
 
 const outfit = Outfit({});
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${outfit.className} antialiased`}>
         <Provider>
-          {children}
+          <NextAuthProvider session={session}>{children}</NextAuthProvider>
           <Toaster />
         </Provider>
       </body>

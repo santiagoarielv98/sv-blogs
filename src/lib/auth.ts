d3dbe3789b073c055e93c3b45300fa84872c6ee7
Santiago Ariel <santiagoarielv98@gmail.com>
2025-01-02 10:53:35 -0300

    Remove username from NextAuth session and JWT handling for simplified user authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,21 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { loginSchema } from "@/schemas/login-schema";
 import { comparePassword } from "@/utils/password";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import type { DefaultSession } from "next-auth";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
-declare module "next-auth" {
-  interface User {
-    username?: string;
-  }
-  interface Session {
-    user: {
-      id: string;
-      username: string;
-    } & DefaultSession["user"];
-  }
-}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -63,13 +50,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.username = user.username;
       }
       return token;
     },
     session({ session, token }) {
       session.user.id = token.id as string;
-      session.user.username = token.username as string;
       return session;
     },
   },

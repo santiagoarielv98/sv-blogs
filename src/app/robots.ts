e3980fa2166d52b783dd8251ccf987f2cba62ead
Santiago Ariel <santiagoarielv98@gmail.com>
2025-01-05 18:33:16 -0300

    Add robots and sitemap generation; implement rules for web crawlers and dynamic sitemap creation for published posts.

---
+++ src/app/robots.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/tags", "/tag/*"],
+        disallow: [
+          "/dashboard",
+          "/login",
+          "/register",
+          "/api/*",
+          "/*/edit",
+          "/new",
+          "/404/*",
+          "/403/*",
+        ],
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  };
+}
